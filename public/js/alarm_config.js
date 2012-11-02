a88dc4b1ec81b20d1bcd7f6d7814bbaa885709c0
Leigh Pauls <leigh.pauls@gmail.com>
2012-11-02 01:07:58 -0700

    Basic config interactions

--- public/js/alarm_config.js
+++ public/js/alarm_config.js
@@ -1,9 +1,23 @@
 function alarm_config_loader(node, instance_cb) {
     var deps = deps_cb(function() { instance_cb(node); });
 
-    loaders.nap_config(node.find(".alm-nap-config-cont"), deps.add_dep());
-    loaders.time_alarm_config(node.find(".alm-time-alarm-config-cont"),
-			      deps.add_dep());
+    var alarm_config = node.find(".alm-time-alarm-config-cont")
+	.css('display', 'none');
+    loaders.time_alarm_config(alarm_config, deps.add_dep());
+
+    var nap_config = node.find(".alm-nap-config-cont")
+	.css('display', 'none');
+    loaders.nap_config(nap_config, deps.add_dep());
+
+    var config_buttons = node.find('.alm-config-buttons');
+    node.find('.alm-time-alarm-button').click(function() {
+	config_buttons.css('display', 'none');
+	alarm_config.css('display', 'inline');
+    });
+    node.find('.alm-nap-alarm-button').click(function() {
+	config_buttons.css('display', 'none');
+	nap_config.css('display', 'inline');
+    });
 
     deps.enable();
 }
